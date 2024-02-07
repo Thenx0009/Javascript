a11e6a12dd2efbe8df23c4c1279368746a42180b
Thenx0009 <ayushverm0009@gmail.com>
2024-02-07 23:47:41 +0530

    first commit

---
+++ Lecture_3_Loops&Strings/third.js
@@ -0,0 +1,45 @@
+//LOOPs
+/**
+ * 1--> for Loop
+ * 2--> while Loop
+ * 
+ * 3--> do{
+ *         
+ *      }while(condition);
+ * 
+ * 4--> for-of loop ; It helps to apply loop on STRINGS and ARRAYS
+ * 5--> for-in loop ; It helps to apply loop on OBJECTS
+ */
+
+
+//do-while example
+// let i = 1;
+// do{
+//     console.log(5*i);
+//     i++;
+// }while(i<=10);
+
+//for-of example
+// let str = "javascript"
+// for (const i of str) {
+//     console.log(i)
+// }
+// let arr = ["apple","mango","banana","guava","grapes"];
+// for (const i of arr) {
+//     console.log(i)
+// }
+
+//for-in loop example
+// const profile = {
+//     username : "thenx09",
+//     followers : 525,
+//     following : 5,
+//     isFollow : false,
+//     posts : 10,
+//     Bio : "giufgruifgruifgr"
+// }
+// for (const key in profile) {
+//     console.log(key+ ":" +profile[key]);
+// }
+
+//STRINGS
