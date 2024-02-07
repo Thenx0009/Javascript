6d734dc11fbce4418054abc184cfbd5acbd84058
Thenx0009 <ayushverm0009@gmail.com>
2024-02-07 21:00:23 +0530

    first commit

---
+++ Lecture_1/first.js
@@ -0,0 +1,21 @@
+//Q1: create a const object called "product"  to store information. 
+const product = {
+    name : "pen",
+    rating : 5,
+    isDeal : true, 
+    offer : "5%",
+    price : 270
+}
+console.log(product)
+
+//Q2: create a const object called "profile" to store information
+
+const profile = {
+    username : "thenx09",
+    followers : 525,
+    following : 5,
+    isFollow : false,
+    posts : 10,
+    Bio : "giufgruifgruifgr"
+}
+console.log(profile)
