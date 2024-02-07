5afbbafacc4b03e52a711eb55a6e73c565ea27cc
Thenx0009 <ayushverm0009@gmail.com>
2024-02-07 22:33:02 +0530

    first commit

--- Lecture_2/second.js
+++ Lecture_2/second.js
@@ -23,20 +23,20 @@
  * 2--> if-else statement
  * 3--> if-(else-if)-else statement
  */
-// let a =5;
-// let b = "5" // string -> number
-// console.log(a==b);
-// console.log(a===b);
+let a =5;
+let b = "5" // string -> number
+console.log(a==b);
+console.log(a===b);
 
 //Q1) Get user  to input  a number  using prompt("Enter a Number: "). Check whether the number is a multiple of 5 or not
 
-// let input = prompt("Enter a number");
-// if(input%5 === 0){
-//     console.log("multile of 5");
-// }
-// else{
-//     console.log("not a multiple of 5");
-// }
+let input = prompt("Enter a number");
+if(input%5 === 0){
+    console.log("multile of 5");
+}
+else{
+    console.log("not a multiple of 5");
+}
 
 //Q2) Write a code which can give  grades  to students  according to their scores
 let score = prompt("Enter the score");
